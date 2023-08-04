1cd2819b40d8487d3e67378fadd2e9c91b8b5502
Fei Hong <feihong0806@gmail.com>
2023-08-04 18:28:22 +0800

    Main Code

---
+++ app/src/main/java/com/example/wanjing/ChooseAdressActivity.java
@@ -0,0 +1,14 @@
+package com.example.wanjing;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ChooseAdressActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_choose_adress);
+    }
+}
--- app/src/main/java/com/example/wanjing/MainActivity.java
+++ app/src/main/java/com/example/wanjing/MainActivity.java
@@ -1,15 +1,31 @@
 package com.example.wanjing;
 
 import androidx.appcompat.app.AppCompatActivity;
-
+import androidx.fragment.app.FragmentPagerAdapter;
+import androidx.viewpager.widget.ViewPager;
 import android.os.Bundle;
-import android.widget.TextView;
+import com.example.wanjing.Adapter.MainAdapter;
+import com.google.android.material.tabs.TabLayout;
 
 public class MainActivity extends AppCompatActivity {
+
+    private TabLayout tabLayout;
+    private ViewPager viewPager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        tabLayout = findViewById(R.id.main_tab);
+        viewPager = findViewById(R.id.viewPage_1);
+
+        tabLayout.setupWithViewPager(viewPager);
+
+        MainAdapter vpAdapter = new MainAdapter(getSupportFragmentManager(), FragmentPagerAdapter.BEHAVIOR_RESUME_ONLY_CURRENT_FRAGMENT);
+        vpAdapter.addfragment(new LoginFragment(), "LOGIN");
+        vpAdapter.addfragment(new SignUpFragment(), "REGISTER");
+        viewPager.setAdapter(vpAdapter);
+
     }
 }
