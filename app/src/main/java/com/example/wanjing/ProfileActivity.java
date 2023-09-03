8e6225fd3359c87f2f9f6c41edaf742986451c08
Fei Hong <feihong0806@gmail.com>
2023-09-03 12:36:50 +0800

    Profile Update

--- app/src/main/java/com/example/wanjing/ProfileActivity.java
+++ app/src/main/java/com/example/wanjing/ProfileActivity.java
@@ -2,13 +2,29 @@ package com.example.wanjing;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class ProfileActivity extends AppCompatActivity {
+
+    private TextView tv_saved;
+    private static final int REQUEST_CODE_MAP = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
+
+        tv_saved = findViewById(R.id.tv_saved_laundry);
+
+        tv_saved.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(ProfileActivity.this, MapsActivity.class);
+                startActivityForResult(intent, REQUEST_CODE_MAP);
+            }
+        });
     }
 }
